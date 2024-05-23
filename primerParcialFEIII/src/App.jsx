8106534a7c597a629c638babb59b179b68714ec6
Ignacio <ignaciosigal04@gmail.com>
2024-05-23 18:45:56 -0300

    primer push

---
+++ primerParcialFEIII/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+
+  return (
+    <div>
+      <h1>Carga de estudiantes</h1>
+      <form action=""></form>
+      <Card></Card>
+    </div>
+  )
+}
+
+export default App
